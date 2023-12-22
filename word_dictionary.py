fdce86af136deedb1b66532a51c22c9ee663c26d
Kunyelewosi Alexander <kunalexander360@gmail.com>
2023-12-22 08:47:13 +0000

    Initial commit

---
+++ word_dictionary.py
@@ -0,0 +1,6 @@
+"""
+# Have a python dictionary that has a key-value pair that represents a word and its definition
+# collect input from the user, input is a word
+# chec if the word is in thhe dictionry
+# print the definition of the word
+"""
