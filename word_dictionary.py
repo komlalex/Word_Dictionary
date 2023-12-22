40408ef1ad8312e502e7f62c74de1962af45e3e7
Kunyelewosi Alexander <kunalexander360@gmail.com>
2023-12-22 09:00:19 +0000

    A demo custom dictionary

--- word_dictionary.py
+++ word_dictionary.py
@@ -4,3 +4,25 @@
 # chec if the word is in thhe dictionry
 # print the definition of the word
 """
+def main():
+    word_dictionary = {
+        "apple": "a fruit",
+        "bible": "the holy book of christians",
+        "hi": "a way of greeting",
+        "eye": "an organ for seeing",
+        "earth": "our plant",
+        "python": "a fun programming language"
+    }
+    
+    while True:
+        word = input("Enter a word: ")
+
+        if word == "":
+         break
+
+        if word in word_dictionary:
+            print(f"{word}: {word_dictionary[word]} ")
+        else:
+           print(f"{word} is not a word in the Alexandrian Dictionary")
+
+main()
